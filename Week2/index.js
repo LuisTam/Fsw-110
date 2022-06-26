cd4eabf59c20a51209f77867d0299d9d7fd11e6e
LuisTam <104099429+LuisTam@users.noreply.github.com>
2022-06-25 18:08:13 -0600

    Add files via upload

---
+++ Week2/index.js
@@ -0,0 +1,3 @@
+document.getElementById("header").innerHTML = "Welcome to my JS site"
+document.getElementById("paragraph").innerHTML = "All of this was created with Javascript"
+document.getElementById("my-list").innerHTML = " 1. water 2.snacks 3.food"
