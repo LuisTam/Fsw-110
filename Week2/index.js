b749f1c3a65a47620313c9aa013a32d5378928e4
LuisTam <104099429+LuisTam@users.noreply.github.com>
2022-10-14 16:18:10 -0600

    Delete index.js

--- Week2/index.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("header").innerHTML = "Welcome to my JS site"
-document.getElementById("paragraph").innerHTML = "All of this was created with Javascript"
-document.getElementById("my-list").innerHTML = " 1. water 2.snacks 3.food"
