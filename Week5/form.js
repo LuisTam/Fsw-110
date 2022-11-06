68072341447f7be5c190ed1a735e7d9e9109be5b
LuisTam <104099429+LuisTam@users.noreply.github.com>
2022-11-05 19:52:31 -0600

    Delete form.js

--- Week5/form.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById('myForm')
-
-form.addEventListener("submit", (event)=>{
-window.alert('First Name is:' + ' ' + form.firstName.value + '\n'+  'Last Name is:' + ' '+ form.lastName.value + '\n'+ 'Email is:' + ' ' + form.email.value)
-document.getElementById("myForm").reset()
-})
