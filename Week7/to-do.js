35372b380cb6950184c3b95da9e547ddaa68ee19
LuisTam <104099429+LuisTam@users.noreply.github.com>
2022-11-19 19:05:48 -0700

    Add files via upload

---
+++ Week7/to-do.js
@@ -0,0 +1,39 @@
+var subBtn = document.getElementById('btn');
+subBtn.addEventListener("click", addChore);
+
+
+function addChore(){
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute('class', "div1")
+    var newLi = document.createElement("li");
+    newLi.setAttribute("id", "listItem")
+    var newChore = document.getElementById("chore").value
+    var desc = document.getElementById("info").value
+    newDiv.appendChild(newLi)
+    
+    newLi.textContent = newChore + " " + desc
+    var htLi = document.getElementById("myList");
+    htLi.append(newLi);
+
+    var button = document.createElement('button');
+    button.className = "deleteBtn"
+    var textContent = document.createTextNode("Delete");
+    button.appendChild(textContent)
+    newLi.appendChild(button)
+
+}
+
+function ClearFields() {
+    document.getElementById("chore").value = "";
+    document.getElementById("info").value = "";
+}
+subBtn.addEventListener("click", ClearFields);
+
+var del = document.getElementsByClassName('deleteBtn');
+del.addEventListener("click", deletedItems);
+
+function deletedItems(){
+ var delItem = document.getElementsByClassName("div1");
+
+ delItem.remove()
+}
